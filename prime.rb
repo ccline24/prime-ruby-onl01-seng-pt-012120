0f312f8b99a569d11e8507a3e12defbb9e737e32
Chance cline <clinechance@gmail.com>
2020-02-03 18:56:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,4 @@
-# def prime(num)
-#   prime.map do |num|
-#     num = prime_num
-#     if prime_num == prime_num.prime
-#       true
-#     else 
-#       false
-# end
-# end
-# end
+
 def prime?(num)
   if num <= 1 
     false
